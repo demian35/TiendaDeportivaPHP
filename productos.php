c7ebcd21eb87bc7235afe9b77fd84a4b9ac3afa8
Demian <53890358+demian35@users.noreply.github.com>
2023-07-24 20:48:04 -0600

    Armando el esqueleto de la pagina de productos

---
+++ productos.php
@@ -0,0 +1,48 @@
+<?php include("template/header.php") ?>
+
+<div class="col-md-3">
+    <div class="card">
+        <img class="card-img-top" src="./img/pumas2029_MED.jpg" alt="Title">
+        <div class="card-body">
+            <h4 class="card-title">Jersey Club Universidad Nacional 2023-2024</h4>
+            <p class="card-text">Jersey Nike Club Universidad Local 2023-2024</p>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver detalles</a>
+        </div>
+    </div>
+</div>
+
+<div class="col-md-3">
+    <div class="card">
+        <img class="card-img-top" src="./img/pumas2029_MED.jpg" alt="Title">
+        <div class="card-body">
+            <h4 class="card-title">Jersey Club Universidad Nacional 2023-2024</h4>
+            <p class="card-text">Jersey Nike Club Universidad Local 2023-2024</p>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver detalles</a>
+        </div>
+    </div>
+</div>
+
+<div class="col-md-3">
+    <div class="card">
+        <img class="card-img-top" src="./img/pumas2029_MED.jpg" alt="Title">
+        <div class="card-body">
+            <h4 class="card-title">Jersey Club Universidad Nacional 2023-2024</h4>
+            <p class="card-text">Jersey Nike Club Universidad Local 2023-2024</p>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver detalles</a>
+        </div>
+    </div>
+</div>
+
+<div class="col-md-3">
+    <div class="card">
+        <img class="card-img-top" src="./img/pumas2029_MED.jpg" alt="Title">
+        <div class="card-body">
+            <h4 class="card-title">Jersey Club Universidad Nacional 2023-2024</h4>
+            <p class="card-text">Jersey Nike Club Universidad Local 2023-2024</p>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver detalles</a>
+        </div>
+    </div>
+</div>
+
+
+<?php include("template/footer.php") ?>
