f721fc98c8aa6b727868d2c121be15fa99568204
Demian <53890358+demian35@users.noreply.github.com>
2023-07-24 20:11:45 -0600

    creando el header de la pagina

---
+++ template/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda Deportiva</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css" />
+</head>
+
+<body>
+
+
+    <nav class="navbar navbar-expand navbar-dark bg-primary">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Deportes Demian </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Inicio</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Playeras</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Acerca de nosotros</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="container">
+        <div class="row">
