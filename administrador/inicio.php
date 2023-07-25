a683b76571e0ad8277a28f08d8f54d2030e0bd1f
Demian <53890358+demian35@users.noreply.github.com>
2023-07-24 22:17:36 -0600

    Agregando la cabecera y el pie de pagina al inicio

--- administrador/inicio.php
+++ administrador/inicio.php
@@ -1,32 +1,4 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS v5.2.1 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-
-</head>
-
-<body>
-
-    <nav class="navbar navbar-expand navbar-light bg-light">
-        <div class="nav navbar-nav">
-            <a class="nav-item nav-link active" href="#" aria-current="page">Administrador del sitio <span class="visually-hidden">(current)</span></a>
-            <a class="nav-item nav-link" href="#">Inicio</a>
-            <a class="nav-item nav-link" href="#">Playeras</a>
-            <a class="nav-item nav-link" href="#">Cerrar sesion</a>
-            <a class="nav-item nav-link" href="#">Ver sitio web</a>
-
-        </div>
-    </nav>
-
-    <div class="container">
-        <div class="row">
+<?php include('template/header.php') ?>
             <div class="col-md-12">
                 <div class="bg-primary p-5 rounded-lg m-3">
                     <h1 class="display-4" style="color: black;"><strong> Bienvenid@</strong></h1>
@@ -35,10 +7,5 @@
                     <p style="color: black;"><strong> podras guardar y ver todos tus proyectos</strong></p>
                 </div>
             </div>
-        </div>
-    </div>
 
-
-</body>
-
-</html>
+<?php include('template/footer.php') ?>          
