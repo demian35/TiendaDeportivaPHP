1971a8657cfaa4e457622c80008f082f7dd83a81
Demian <53890358+demian35@users.noreply.github.com>
2023-07-24 19:28:59 -0600

    Provando estilos de bootstrap en la pg principal

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-echo "Hello world"."<br>"
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -8,8 +5,20 @@ echo "Hello world"."<br>"
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda Deportiva</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css"/>
 </head>
 <body>
-    Deportes Demian muy pronto
+    
+
+    <nav class="navbar navbar-expand navbar-dark bg-primary">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="#" aria-current="page">Nav 1 <span class="visually-hidden">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nav 2</a>
+            </li>
+        </ul>
+    </nav>
 </body>
 </html>
