965393a47701fb88a7cd420453b65cc5c6868d1a
Demian <53890358+demian35@users.noreply.github.com>
2023-07-24 19:06:59 -0600

    Creando la pagina principal de la pagina

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+echo "Hello world"."<br>"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda Deportiva</title>
+</head>
+<body>
+    Deportes Demian muy pronto
+</body>
+</html>
