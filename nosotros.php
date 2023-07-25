82b5161986404e6260393f5f0de72435c4d6d185
Demian <53890358+demian35@users.noreply.github.com>
2023-07-24 20:20:26 -0600

    Creando la pagina de acerca de nosotros

---
+++ nosotros.php
@@ -0,0 +1,9 @@
+<?php include("template/header.php") ?>
+
+<div class="bg-primary p-5 rounded-lg m-3">
+    <h1 class="display-4" style="color: black;"><strong> Acerca de Nosotros</strong></h1>
+    <p class="lead" style="color: black;"><strong> Hola nosotros somos un grupo apacionado por la programacion web y los deportes en especial el futbol</strong></p>
+    <hr class="my-4" style="color: black;">
+</div>
+
+<?php include("template/footer.php") ?>
