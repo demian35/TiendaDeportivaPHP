6f94b5a0f527f3b1f4524e97567d34017506ad80
Demian <53890358+demian35@users.noreply.github.com>
2023-07-24 21:21:45 -0600

    Interfas del login del admin

--- administrador/index.php
+++ administrador/index.php
@@ -1,11 +1,60 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Administracion de la pagina</title>
+    <title>Inicia Sesion</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <style>
+         body{
+            background-image: url('../img/estadio01.jpg');
+            background-size: 1370px 1100px;
+            background-position: center;
+        }
+    </style>
 </head>
+
 <body>
-    hello world
+    <div class="container">
+        <div class="row">
+
+        <div class="col-md-4">
+
+        </div>
+            <div class="col-md-4">
+                <br>
+                <div class="card">
+                    <div class="card-header">
+                        Login
+                    </div>
+                    <div class="card-body">
+                        <form>
+
+                        <div class = "form-group">
+                        <label for="exampleInputEmail1">Usuario</label>
+                        <input required type="text" class="form-control" name="usuario" aria-describedby="emailHelp" placeholder="Ingrese su usuario">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+
+                        <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input required type="password" class="form-control" name="contrasena" placeholder="Password">
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Iniciar sesion</button>
+                        </form>
+                        
+                        
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
