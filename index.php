00acca81a132c7152c8a6f358fb0d29a9bdd55c0
Demian <53890358+demian35@users.noreply.github.com>
2023-07-24 20:12:13 -0600

    Agregando el header y el footer al index

--- index.php
+++ index.php
@@ -1,46 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tienda Deportiva</title>
-    <link rel="stylesheet" href="./css/bootstrap.min.css" />
-</head>
-
-<body>
-
-
-    <nav class="navbar navbar-expand navbar-dark bg-primary">
-        <ul class="nav navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link active" href="#">Deportes Demian </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Inicio</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="#">Playeras</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="#">Acerca de nosotros</a>
-            </li>
-        </ul>
-    </nav>
-
-
-    <div class="container">
-        <div class="row">
+<?php include("template/header.php") ?>
             <div class="bg-primary p-5 rounded-lg m-3">
                 <h1 class="display-4" style="color: black;"><strong> Bienvenid@</strong></h1>
                 <p class="lead" style="color: black;"><strong> Esta es tu galeria privada.</strong></p>
                 <hr class="my-4" style="color: black;">
                 <p style="color: black;"><strong> podras guardar y ver todos tus proyectos</strong></p>
             </div>
-        </div>
-    </div>
-</body>
-
-</html>
+<?php include("template/footer.php") ?>
