560c52a54ab646e080c430f440c9d7af16959a73
Demian <53890358+demian35@users.noreply.github.com>
2023-07-24 23:09:24 -0600

    Cambiando textos del jumbotron del inicio

--- administrador/inicio.php
+++ administrador/inicio.php
@@ -2,9 +2,9 @@
             <div class="col-md-12">
                 <div class="bg-primary p-5 rounded-lg m-3">
                     <h1 class="display-4" style="color: black;"><strong> Bienvenid@</strong></h1>
-                    <p class="lead" style="color: black;"><strong> Esta es tu galeria privada.</strong></p>
+                    <p class="lead" style="color: black;"><strong> Administrador </strong></p>
                     <hr class="my-4" style="color: black;">
-                    <p style="color: black;"><strong> podras guardar y ver todos tus proyectos</strong></p>
+                    <p style="color: black;"><strong> Aqui podras gestionar todo lo que tenga que ver con los productos que vendemos en nuestra tienda </strong></p>
                 </div>
             </div>
 
