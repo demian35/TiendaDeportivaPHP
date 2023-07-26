205f112ef716ed54b4f3ae35615249af398ee8e0
Demian <53890358+demian35@users.noreply.github.com>
2023-07-26 02:32:10 -0600

    mostrando productos al usuario, proyecto terminado

--- productos.php
+++ productos.php
@@ -1,48 +1,26 @@
 <?php include("template/header.php") ?>
+<?php include("administrador/config/conexionBD.php"); 
 
+//sentencia para mostrar los productos en la pantalla
+
+$sentencia = $conexion->prepare("SELECT * FROM sitiotiendadeportiva.productos;");
+$sentencia->execute(); //ejecutamos la sentencia
+$resultados = $sentencia->fetchAll(PDO::FETCH_ASSOC); //mostramos los registros en la pantalla
+
+
+?>
+<?php foreach($resultados as $producto) {?>
 <div class="col-md-3">
     <div class="card">
-        <img class="card-img-top" src="./img/pumas2029_MED.jpg" alt="Title">
+        <img class="card-img-top" src="./img/<?php echo $producto['imagen']?>" alt="Title">
         <div class="card-body">
-            <h4 class="card-title">Jersey Club Universidad Nacional 2023-2024</h4>
-            <p class="card-text">Jersey Nike Club Universidad Local 2023-2024</p>
-            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver detalles</a>
+            <h4 class="card-title"><?php echo $producto['producto']?></h4>
+            
+            <a name="" id="" class="btn btn-primary" href="https://www.kitfutboll.com/camisetas-de-futbol-club-america-primera-20232024-p-25627.html" role="button">Ver detalles</a>
         </div>
     </div>
 </div>
-
-<div class="col-md-3">
-    <div class="card">
-        <img class="card-img-top" src="./img/pumas2029_MED.jpg" alt="Title">
-        <div class="card-body">
-            <h4 class="card-title">Jersey Club Universidad Nacional 2023-2024</h4>
-            <p class="card-text">Jersey Nike Club Universidad Local 2023-2024</p>
-            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver detalles</a>
-        </div>
-    </div>
-</div>
-
-<div class="col-md-3">
-    <div class="card">
-        <img class="card-img-top" src="./img/pumas2029_MED.jpg" alt="Title">
-        <div class="card-body">
-            <h4 class="card-title">Jersey Club Universidad Nacional 2023-2024</h4>
-            <p class="card-text">Jersey Nike Club Universidad Local 2023-2024</p>
-            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver detalles</a>
-        </div>
-    </div>
-</div>
-
-<div class="col-md-3">
-    <div class="card">
-        <img class="card-img-top" src="./img/pumas2029_MED.jpg" alt="Title">
-        <div class="card-body">
-            <h4 class="card-title">Jersey Club Universidad Nacional 2023-2024</h4>
-            <p class="card-text">Jersey Nike Club Universidad Local 2023-2024</p>
-            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver detalles</a>
-        </div>
-    </div>
-</div>
+<?php } ?>
 
 
 <?php include("template/footer.php") ?>
