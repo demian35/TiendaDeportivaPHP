c3305df4645b935a95017761fa64c48fa573c003
Demian <53890358+demian35@users.noreply.github.com>
2023-07-26 02:01:56 -0600

    decorando inicio

--- administrador/inicio.php
+++ administrador/inicio.php
@@ -1,10 +1,11 @@
 <?php include('template/header.php') ?>
             <div class="col-md-12">
                 <div class="bg-primary p-5 rounded-lg m-3">
-                    <h1 class="display-4" style="color: black;"><strong> Bienvenid@</strong></h1>
+                    <h1 class="display-4" style="color: black;"><strong> <?php echo "Bienvenid@"."".$nombreUsuario ?> </strong></h1>
                     <p class="lead" style="color: black;"><strong> Administrador </strong></p>
                     <hr class="my-4" style="color: black;">
                     <p style="color: black;"><strong> Aqui podras gestionar todo lo que tenga que ver con los productos que vendemos en nuestra tienda </strong></p>
+                    <a class="btn btn-warning ntn-lg" href="seccion/productos.php" role="button">Ver inventario de productos</a>
                 </div>
             </div>
 
