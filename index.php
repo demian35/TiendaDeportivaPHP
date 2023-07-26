fbf4acf9706ef3417272fc2aff1aa4b0cbb13a44
Demian <53890358+demian35@users.noreply.github.com>
2023-07-26 02:31:16 -0600

    decorando el index del usuario

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php include("template/header.php") ?>
             <div class="bg-primary p-5 rounded-lg m-3">
-                <h1 class="display-4" style="color: black;"><strong> Bienvenid@</strong></h1>
-                <p class="lead" style="color: black;"><strong> Esta es tu galeria privada.</strong></p>
+                <h1 class="display-4" style="color: black;"><strong> Bienvenid@s</strong></h1>
+                <p class="lead" style="color: black;"><strong> Esta es una tienda de ropa de futbol en linea.</strong></p>
                 <hr class="my-4" style="color: black;">
-                <p style="color: black;"><strong> podras guardar y ver todos tus proyectos</strong></p>
+                <p style="color: black;"><strong> aqui podras conprar muchas playeras de muchos equipos de futbol del mundo</strong></p>
+                <a class="btn btn-info btn-lg" href="productos.php" role="button">Ver articulos disponibles</a>
             </div>
 <?php include("template/footer.php") ?>
